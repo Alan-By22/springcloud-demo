02860bf61420ff8924a0c9c2371d5a5bc5e49808
zby <zby@qq.com>
2023-01-12 20:10:48 +0800

    nacos的注册与分级模块,权重,热部署

---
+++ user-service/src/main/java/cn/itcast/user/properties/PatternProperties.java
@@ -0,0 +1,20 @@
+package cn.itcast.user.properties;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * @className: PatternProperties
+ * @author: Mr.BingYu
+ * @description:
+ * @date: 2023/01/12 20:04
+ **/
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "pattern")
+public class PatternProperties {
+
+    private String dateformat;
+}
--- user-service/src/main/java/cn/itcast/user/web/UserController.java
+++ user-service/src/main/java/cn/itcast/user/web/UserController.java
@@ -1,18 +1,38 @@
 package cn.itcast.user.web;
 
 import cn.itcast.user.pojo.User;
+import cn.itcast.user.properties.PatternProperties;
 import cn.itcast.user.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Slf4j
 @RestController
 @RequestMapping("/user")
+//@RefreshScope  //热更新
 public class UserController {
 
     @Autowired
     private UserService userService;
+
+    //@Value("${pattern.dateformat}")
+    //private String dateformat;
+
+    @Autowired
+    private PatternProperties patternProperties;
+
+    @GetMapping("now")
+    public String now() {
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(now);
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern(patternProperties.getDateformat()));
+    }
 
     /**
      * 路径： /user/110

bc1da7dbcacbb61c2f420840065ed6da1c86460f
zby <zby@qq.com>
2023-01-12 17:51:59 +0800

    springcloud--Eureka注册中心--负载均衡

---
+++ eureka-server/src/main/java/com/ithuipu/EurekaApplication.java
@@ -0,0 +1,20 @@
+package com.ithuipu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @className: EurekaApplication
+ * @author: Mr.BingYu
+ * @description:
+ * @date: 2023/01/12 16:15
+ **/
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
---
+++ order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -0,0 +1,33 @@
+package cn.itcast.order.service;
+
+import cn.itcast.order.mapper.OrderMapper;
+import cn.itcast.order.pojo.Order;
+import cn.itcast.order.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private OrderMapper orderMapper;
+
+    /**
+     * 注入RestTemplate
+     */
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public Order queryOrderById(Long orderId) {
+        // 1.查询订单
+        Order order = orderMapper.findById(orderId);
+        // 2.url
+        String url = "http://userservice/user/"+order.getUserId();
+        // 3.使用restTemplate---user
+        User user = restTemplate.getForObject(url, User.class);
+        // 4.返回
+        order.setUser(user);
+        return order;
+    }
+}

2b6d8bdf4824bb862fc684e43f350e3b0aadd5d3
zby <zby@qq.com>
2023-01-30 17:53:00 +0800

    网关服务的搭建

---
+++ gateway/src/main/java/com/ithuipu/GatewayApplication.java
@@ -0,0 +1,19 @@
+package com.ithuipu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @className: gatewayApplication
+ * @author: Mr.BingYu
+ * @description:
+ * @date: 2023/01/30 16:01
+ **/
+
+
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class,args);
+    }
+}
